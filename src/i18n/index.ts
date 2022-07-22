2b476e7e482e023e3b20bda3314b3540e69fcc02
zajdawid <zajdawid@protonmail.com>
2022-07-22 16:21:58 +0200

    Add: warn on missing translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
-import {AppLang} from 'types/enums';
+import { initReactI18next } from 'react-i18next';
+import { ENVIRONMENTS } from 'config/constants';
+import { AppLang } from 'types/enums';
 
 export const langs = {
   [AppLang.EN]: require('i18n/lang/en.json').default,
@@ -12,6 +13,14 @@ i18n.use(initReactI18next).init({
   fallbackLng: AppLang.EN,
   interpolation: {
     escapeValue: false,
+  },
+  saveMissing: true,
+  missingKeyHandler: (ng, ns, key, fallbackValue) => {
+    if (process.env.NODE_ENV !== ENVIRONMENTS.PRODUCTION) {
+      console.warn(
+        `Translation hasn't been found for key "${key}" (ng: "${ng}", ns: "${ns}", fallbackValue: "${fallbackValue}")`,
+      );
+    }
   },
 });
 
